8ccc9d19740553a7e00d21839074f984cd7d47d3
Alex Power <alexpower9@gmail.com>
2023-12-23 14:55:51 -0500

    Added the double inside button on the homepage and routing for di

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,17 +2,22 @@
   <div class="home">
     <img class="bobcat-image" alt="Bobcat" src="../assets/newBobcat.jpeg">
     <MyBobcat class="welcome-message" msg="Welcome to the Quinnipiac Mens Ice Hockey Play Page!"></MyBobcat>
+    <div class = "button-container">
+      <DoubleInside></DoubleInside>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import MyBobcat from '@/components/MyBobcat.vue'
+import DoubleInside from '@/components/DoubleInside.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    MyBobcat
+    MyBobcat,
+    DoubleInside
   }
 }
 </script>
@@ -21,6 +26,10 @@ export default {
 .bobcat-image {
   width: 75px;
   height: 75px;
+}
+.button-container{
+  margin-left:auto;
+  margin-right:auto;
 }
 </style>
 
