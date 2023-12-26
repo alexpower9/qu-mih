230f8e1bd6feb4ed8660c8ce15821f3356e429c1
Alex Power <alexpower9@gmail.com>
2023-12-26 13:35:10 -0500

    Tooltips are now working, players are now added to the screen via the Player component.

---
+++ src/components/HockeyPlayer.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="player" :class="{'team-1': team === 1, 'team-2': team === 2}">
+        <VueCustomTooltip :label="tooltipText" class="custom-tooltip">
+            <p>{{ position }}</p>
+        </VueCustomTooltip>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HockeyPlayer',
+    props: {
+        position: {
+            type: String,
+            required: true
+        },
+        team: {
+            type: Number,
+            required: true
+        },
+        tooltipText: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+.player{
+    font-size: 2em;
+    font-weight: bold;
+}
+.team-1{
+    color: #1a4386;
+}
+.team-2{
+    color: #ec0229;
+}
+.custom-tooltip{
+    z-index: 10000 !important;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueCustomTooltip from '@adamdehaven/vue-custom-tooltip'
 
-createApp(App).use(router).mount('#app')
+const opt = {
+    name: 'VueCustomTooltip',
+    zIndex: 1000000,
+    color: '#fff',
+    background: '#000',
+    borderRadius: 100,
+    fontWeight: 400,
+  }
+
+const app = createApp(App)
+
+
+app.use(router).use(VueCustomTooltip,opt).mount('#app')
