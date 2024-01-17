f49f6f6e39d4f78e90bff939c84445928e7ece69
Alex Power <alexpower9@gmail.com>
2024-01-16 21:22:22 -0500

    Added web hash history or something

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import DoubleInsideView from '../views/DoubleInsideView.vue'
 import DoriaView from '../views/DoriaView.vue'
@@ -72,7 +72,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(process.env.BASE_URL),
   routes
 })
 
